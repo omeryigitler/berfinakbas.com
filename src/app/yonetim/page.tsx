2dd2db0cbf12721ebf7ebf65c59b2386b0bef073
Omer YIGITLER <138784969+omeryigitler@users.noreply.github.com>
2026-07-20 18:12:39 +0200

    chore: finalize dashboard production deploy

--- src/app/yonetim/page.tsx
+++ src/app/yonetim/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation';
 
-// Yönetim kökü, görsel kabuğu değiştirmeden canlı başlangıç ekranına yönlenir.
 export default function YonetimPage() {
   redirect('/yonetim/baslangic');
 }
